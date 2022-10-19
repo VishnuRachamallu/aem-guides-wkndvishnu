1e757a51699e1ef3c61985bcb1cc6cfcd2a1f6ae
Vishnu Rachamallu <vitturachamallu001@gmail.com>
2022-10-19 14:44:40 +0530

    added schedulers

---
+++ core/src/main/java/com/adobe/aem/guides/vishnu/core/schedulers/SchedulerJob.java
@@ -0,0 +1,58 @@
+package com.adobe.aem.guides.vishnu.core.schedulers;
+
+import org.apache.sling.commons.scheduler.Job;
+import org.apache.sling.commons.scheduler.JobContext;
+import org.apache.sling.commons.scheduler.ScheduleOptions;
+import org.apache.sling.commons.scheduler.Scheduler;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.Designate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Designate(ocd = SchedulerConfigurationJob.class)
+@Component(service = Job.class,immediate = true)
+public class SchedulerJob implements Job{
+
+	private static final Logger LOG=LoggerFactory.getLogger(SchedulerJob.class);
+	
+	@Reference
+	Scheduler scheduler;
+	
+	private int schedulerId;
+	
+	@Activate
+	public void activate(SchedulerConfiguration configuration) {
+		schedulerId = configuration.schedulerName().hashCode();
+		addScheduler(configuration);
+	}
+
+	@Deactivate
+	public void diactivate(SchedulerConfiguration configuration) {
+		removeScheduler();
+	}
+
+	private void addScheduler(SchedulerConfiguration configuration) {
+		// TODO Auto-generated method stub
+		ScheduleOptions scheduleOptions = scheduler.EXPR(configuration.cornExpression());
+		scheduler.schedule(this, scheduleOptions);
+		LOG.info("\n ===== Vishnu Wknd Scheduler JOB  added========== \n");
+
+	}
+
+	private void removeScheduler() {
+		// TODO Auto-generated method stub
+		scheduler.unschedule(String.valueOf(schedulerId));
+
+	}
+	
+	@Override
+	public void execute(JobContext jobContext) {
+		// TODO Auto-generated method stub
+		LOG.info("\n ===== Execute Vishnu Wknd  JOB Method========== \n");
+		
+	}
+
+}
---
+++ core/src/main/java/com/adobe/aem/guides/vishnu/core/schedulers/SchedulerRunnable.java
@@ -0,0 +1,70 @@
+package com.adobe.aem.guides.vishnu.core.schedulers;
+
+import org.apache.sling.commons.scheduler.ScheduleOptions;
+import org.apache.sling.commons.scheduler.Scheduler;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.wcm.webservicesupport.Service;
+
+@Designate(ocd = SchedulerConfiguration.class)
+@Component(service = Runnable.class, immediate = true)
+public class SchedulerRunnable implements Runnable {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SchedulerRunnable.class);
+	private int schedulerId;
+
+	@Reference
+	Scheduler scheduler;
+
+	@Activate
+	public void activate(SchedulerConfiguration configuration) {
+		schedulerId = configuration.schedulerName().hashCode();
+		addScheduler(configuration);
+	}
+
+	@Deactivate
+	public void diactivate(SchedulerConfiguration configuration) {
+		removeScheduler();
+	}
+
+	private void addScheduler(SchedulerConfiguration configuration) {
+		// TODO Auto-generated method stub
+		ScheduleOptions scheduleOptions = scheduler.EXPR(configuration.cornExpression());
+		scheduleOptions.name(String.valueOf(schedulerId));
+		scheduleOptions.canRunConcurrently(false);
+		scheduler.schedule(this, scheduleOptions);
+		LOG.info("\n ===== Vishnu Wknd Scheduler 1  added========== \n");
+		ScheduleOptions scheduleOptions2 = scheduler.NOW();
+		scheduler.schedule(this, scheduleOptions2);
+
+		/*
+		 * This will run scheduler immediately and two more times in interval of 5
+		 * seconds
+		 * 
+		 * ScheduleOptions scheduleOptions3=scheduler.NOW(3,5);
+		 * scheduler.schedule(this,scheduleOptions3);
+		 */
+
+	}
+
+	private void removeScheduler() {
+		// TODO Auto-generated method stub
+		scheduler.unschedule(String.valueOf(schedulerId));
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+		LOG.info("\n ===== Vishnu Wknd Run Method========== \n");
+	}
+
+}
