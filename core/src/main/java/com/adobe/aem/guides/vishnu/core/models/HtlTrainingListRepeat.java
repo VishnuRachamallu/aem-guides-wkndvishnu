f68daaa503c8861a100faf0ce94969102a2dd7f7
Vishnu Rachamallu <vitturachamallu001@gmail.com>
2022-10-27 15:02:50 +0530

    fsdfvd

--- core/src/main/java/com/adobe/aem/guides/vishnu/core/models/HtlTrainingListRepeat.java
+++ core/src/main/java/com/adobe/aem/guides/vishnu/core/models/HtlTrainingListRepeat.java
@@ -3,6 +3,7 @@ package com.adobe.aem.guides.vishnu.core.models;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -11,13 +12,18 @@ import javax.inject.Named;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import com.adobe.aem.guides.vishnu.core.helper.MultifieldHelper;
 import com.adobe.aem.guides.vishnu.core.models.interfa.AuthorBooks;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
 
 @Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 
@@ -34,7 +40,11 @@ public class HtlTrainingListRepeat {
 	@Named("booksauthor")
 	private List<String> books;
 
-	// author
+	@ScriptVariable
+	PageManager pageManager;
+
+	@SlingObject
+	ResourceResolver resourceResolver;
 
 	public String getAuthorName() {
 		return author;
@@ -65,10 +75,10 @@ public class HtlTrainingListRepeat {
 
 		return totalList;
 	}
-	
-	public Map<String,String> getSampleMap(){
-		Map<String,String> sMap=new HashMap<>();
-		
+
+	public Map<String, String> getSampleMap() {
+		Map<String, String> sMap = new HashMap<>();
+
 		sMap.put("Key1", "Value1");
 		sMap.put("Key2", "Value2");
 		sMap.put("Key3", "Value3");
@@ -76,8 +86,29 @@ public class HtlTrainingListRepeat {
 		sMap.put("Key5", "Value5");
 		sMap.put("Key6", "Value6");
 		sMap.put("Key7", "Value7");
-		
+
 		return sMap;
+	}
+
+	public List<Page> getPages() {
+
+		List<Page> childPage = new ArrayList<>();
+		Page home = pageManager.getPage("/content/vishnuwknd/us/events");
+		Iterator<Page> childs = home.listChildren();
+
+		while (childs.hasNext()) {
+			Page page = childs.next();
+			childPage.add(page);
+		}
+		return childPage;
+	}
+	
+	public Iterator<Page> getPagesIterator() {
+
+		List<Page> childPage = new ArrayList<>();
+		Page home = pageManager.getPage("/content/vishnuwknd/us/events");
+		Iterator<Page> childs = home.listChildren();
+		return childs;
 	}
 
 }
